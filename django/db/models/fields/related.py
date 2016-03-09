f405e9e8c4180b9f3a242eefc1027a33bce918cb
Vadym Zakovinko <vadym@zakovinko.com>
2016-03-09 14:42:50 +0200

    task-9391 don't clear related on set

--- django/db/models/fields/related.py
+++ django/db/models/fields/related.py
@@ -1196,8 +1196,24 @@ class ReverseManyRelatedObjectsDescriptor(object):
         # clear() can change expected output of 'value' queryset, we force evaluation
         # of queryset before clear; ticket #19816
         value = tuple(value)
-        manager.clear()
-        manager.add(*value)
+
+        ############################################################
+        # Delete unused objects by remove not clear                #
+        # solution the same as for ForeignRelatedObjectsDescriptor #
+        # needs for InsuranceRequestDetail & InsuranceRequestOffer #
+        ############################################################
+
+        existing_objects = set(manager.all())
+        new_objects = set(value)
+
+        # define objects that have to be added / removed
+        objects_to_remove = existing_objects - new_objects
+        objects_to_add = new_objects - existing_objects
+
+        for obj in objects_to_remove:
+            obj.remove()
+
+        manager.add(*objects_to_add)
 
 
 class ForeignObjectRel(object):
